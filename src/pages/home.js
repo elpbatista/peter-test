6b7fd04aea5a49f38e02870e7ad2f16eabbc81b7
elpbatista <elpbatista@gmail.com>
2022-06-20 14:50:28 -0700

    this is a test

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Test from "../components/test";
+import { useTranslation } from "react-i18next";
 
-const Home = () => (<><Test /></>);
+function Home() {
+  const { t, i18n } = useTranslation();
+  return (
+    <>
+      <Test />
+      <h2>{t("Welcome to React")}</h2>
+    </>
+  );
+}
 
 export default Home;
